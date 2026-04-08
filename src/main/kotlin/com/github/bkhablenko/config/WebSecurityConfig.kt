9ace60100700f8e55aa851c4fc9740a2f2053285
Bohdan Khablenko <bkhablenko@gmail.com>
2026-04-08 16:44:55 +0300

    Disable HTTP session creation

--- src/main/kotlin/com/github/bkhablenko/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/github/bkhablenko/config/WebSecurityConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
@@ -49,6 +50,9 @@ class WebSecurityConfig(private val cors: CorsConfigProperties) : WebMvcConfigur
                 disable()
             }
             httpBasic {}
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
         }
         return http.build()
     }
